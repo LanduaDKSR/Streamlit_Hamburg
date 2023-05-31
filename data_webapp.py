6da54456c426cb9b40f3983ca13f76eb96c08a46
LanduaDKSR <fabian.landua@dksr.city>
2023-05-31 09:39:11 +0200

    new mockup

--- data_webapp.py
+++ data_webapp.py
@@ -7,14 +7,21 @@ from streamlit_keplergl import keplergl_static
 from keplergl import KeplerGl
 from geojson import LineString as geoLS
 import geopandas
-#import requests
+import requests
 #import json
 import datetime
 import ast
-#import map_config
+import scooter_config
+import stations_config
+
+from PIL import Image
 
 st.set_page_config(layout="wide")
-#config = map_config.config
+
+image = Image.open('Logo.png')
+
+config = scooter_config.config
+config2 = stations_config.config
 
 def trip_layer(data):
     geo_list = pd.DataFrame(np.zeros((len(data)),dtype=object),columns=['geo_json'])
@@ -33,8 +40,64 @@ def trip_layer(data):
     return geo_list
 
 
+def rental_stations():
+    entity = 'https://iot.hamburg.de/v1.1/Things?'
+    rows = '$skip=0&$top=5000&'
+    #gefiltert nach Fahrradzählstationen
+    filter = '$filter=((properties%2FownerThing+eq+%27Hamburg+Verkehrsanlagen%27))'
+    filter_observation = '?$skip=0&$top=1&$orderby=resultTime+desc'
 
-@st.cache_data
+    url = entity + rows + filter
+    response = requests.get(url)
+    things_data = response.json()
+
+    # get a list of datastream_link from every thing
+    datastreams = []
+    for thing in things_data['value']:
+        thing = thing['Datastreams@iot.navigationLink'] 
+        datastreams.append(thing)
+
+    ## get a list of datastreams from every thing
+    # all links with last 3 observations
+    observations = []
+    for stream in datastreams:
+        stream = requests.get(stream)
+        data = stream.json()
+        value = data['value']
+        link = value[0]['Observations@iot.navigationLink']+filter_observation
+        observations.append(link)
+
+    final_df = pd.DataFrame() 
+    result_list = []
+    resultTime_list = []
+    coordinates_list = []
+
+    for observation in observations:
+        request = requests.get(observation)
+        data = request.json()
+        values = data['value']
+
+        for item in values:
+            # result = counts and resulttime = time
+            result_list.append(item['result'])
+            resultTime_list.append(item['resultTime'])
+
+            # holding coordinates
+            link = item['FeatureOfInterest@iot.navigationLink']
+
+            # Request 'FeatureOfInterest@iot.navigationLink'
+            request = requests.get(link)
+            feature_of_interest = request.json()
+            coordinates = feature_of_interest['feature']['geometry']['coordinates']
+            coordinates_list.append(coordinates)
+
+    df = pd.DataFrame({'result': result_list, 'resultTime': resultTime_list, 'coordinates': coordinates_list})
+    df['lon'] = df['coordinates'].apply(lambda x: x[0])
+    df['lat'] = df['coordinates'].apply(lambda x: x[1])
+    return df
+
+
+#@st.cache_data
 def load_data(source1, source2):   
     df = pd.read_csv(source1)
     df.drop(['Unnamed: 0'], axis=1, inplace=True)
@@ -48,11 +111,12 @@ def load_data(source1, source2):
     gdf = gdf.to_crs("EPSG:4326")
     stations['lon'] = gdf['geometry'].x
     stations['lat'] = gdf['geometry'].y
+    stations['icon'] = pd.DataFrame(['place'] * len(stations))
     stations.reset_index(inplace=True)
     stations.drop(['index'], axis=1, inplace=True)
 
     #df = pd.concat([df, trip_layer(df)], axis=1)
-    map_1 = KeplerGl(height=500, data = {'Scooters': df, 'stations': stations[['Name', 'lon', 'lat']]})
+    map_1 = KeplerGl(height=650, data = {'Scooters': df, 'stations': stations[['Name', 'lon', 'lat', 'icon']]}, config=config)
     return df, map_1
 
 
@@ -68,6 +132,7 @@ source2 = "c:/Users/FabianLandua/Documents/Data reports/Hamburg/HVV-Haltestellen
 df, map_1 = load_data(source1, source2)
 #hvv, map2 = stations_load(source2)
 
+st.sidebar.write("""Filtern der Daten möglich""")
 d_1 = st.sidebar.date_input('Start', datetime.date(2023,2,1))
 d_2 = st.sidebar.date_input('Ende', datetime.date(2023,2,28))
 
@@ -75,35 +140,47 @@ d_2 = st.sidebar.date_input('Ende', datetime.date(2023,2,28))
 #df_filtered = data_filter(df,dates)
 #config['config']['visState']['filters'][0]['value'] = [dates[0].day, dates[1].day]
 
+header1, header2 = st.columns([3,1])
+with header1:
+    st.write("""
+    # Urban Data Challenge Hamburg
+    Mockup
+    """)
 
-st.write("""
-# My first app
-Hello *world!*
-LOGO
-""")
+with header2:
+    st.image(image)
 
 
-col1, col2 = st.columns([2,1])
-with col1:
+#st.dataframe(df_filtered)
 
-    #map_1.config = config
-    keplergl_static(map_1)
+tab1, tab2 = st.tabs(["Scooter", "Leihstationen"])
+
+with tab1:
+    col1, col2 = st.columns([3,1])
+    with col1:
+        #map_1.config = config
+        keplergl_static(map_1)
+    with col2:
+        st.write("""Für diese Visualisierung wurden virtuellen Trip-Daten erzeugt. Die Daten bilden keine realen Trips ab und dienen rein zur Veranschaulichung der Darstellbarkeit.""")
 
     st.write("""
-    ## Rohdaten
+    ### Rohdaten
     """)
-    #st.dataframe(df_filtered)
+    st.dataframe(df)
 
-with col2:
-    tab1, tab2 = st.tabs(["Fahrstrecke", "Fahrtzeit"])
-"""
-    with tab1:
-        fig = plt.figure(figsize=(10, 6))
-        sns.histplot(x='trip_distance', data=df_filtered, bins=50, kde=True)
-        st.pyplot(fig)
+with tab2:
+    col1, col2 = st.columns([3,1])
+    with col1:
+        #map_1.config = config
+        rental = rental_stations()
+        keplergl_static(KeplerGl(height=650, data={'Stationen': rental} ,config=config2))
+    with col2:
+        st.write("""Aktuelle Verfügbarkeit an Fahrrad-Leihstationen.
+        Auf Knopfdruck lässt sich die aktuelle Situation anzeigen. (Wartezeit ca. 1 Minute)""")
+        st.button('Auslastung Berechnen')
 
-    with tab2:
-        fig2 = plt.figure(figsize=(10, 6))
-        sns.histplot(x='trip_duration', data=df_filtered, bins=50, kde=True)
-        st.pyplot(fig2)
-"""
+    st.write("""
+    ### Rohdaten
+    """)
+    st.dataframe(rental)
+
